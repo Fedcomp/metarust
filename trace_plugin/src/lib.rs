911e871fc346ea6dcb6a02d587e3e72995f62872
Fedcomp <aglergen@gmail.com>
2020-04-05 10:32:27 +0300

    Start from scratch

--- trace_plugin/src/lib.rs
+++ trace_plugin/src/lib.rs
@@ -1,24 +1,5 @@
-use metarust::{register_plugin, CallResult, Forwards, PluginInfo, PLUG_LOADTIME};
+use hlsdk_sys::{BOOL, TRUE};
 
-const PLUGIN_INFO: PluginInfo = PluginInfo::new(
-    env!("CARGO_PKG_NAME"),
-    env!("CARGO_PKG_VERSION"),
-    env!("CARGO_DATE"),
-    env!("CARGO_PKG_AUTHORS"),
-    env!("CARGO_PKG_HOMEPAGE"),
-    "METARUST",
-    PLUG_LOADTIME::PT_CHANGELEVEL,
-    PLUG_LOADTIME::PT_CHANGELEVEL,
-);
-
-const FORWARDS: Forwards = Forwards {
-    plugin_init: Some(plugin_init),
-};
-
-register_plugin!(PLUGIN_INFO, FORWARDS);
-
-fn plugin_init() -> CallResult {
-    println!("PLUGIN_INIT Activated");
-
-    CallResult::Ignored
+pub extern "C" fn Meta_Detach() -> BOOL {
+    TRUE
 }
