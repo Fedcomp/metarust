be9a5f403f1bd8565f8b6f08e96ef1249cb2c22b
Fedcomp <aglergen@gmail.com>
2020-04-11 04:05:09 +0300

    Remove redudant pragma

--- trace_plugin/src/lib.rs
+++ trace_plugin/src/lib.rs
@@ -1,17 +1,14 @@
 use hlsdk_sys::{BOOL, TRUE};
 
-#[allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 #[no_mangle]
 pub extern "C" fn Meta_Detach(/* TODO */) -> BOOL {
     TRUE
 }
 
-#[allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 #[no_mangle]
 pub extern "C" fn Meta_Query(/* TODO */) -> BOOL {
     TRUE
 }
 
-#[allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 #[no_mangle]
 pub unsafe extern "C" fn GiveFnptrsToDll(/* TODO */) {}
